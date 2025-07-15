760da6d91432557b90c1ee1bee6781729ff22148
KevinV1602 <600488@student.belgiumcampus.ac.za>
2025-07-15 18:24:52 +0200

    First Commit

---
+++ PRG381_Milestone2/src/prg381_milestone2/PRG381_Milestone2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prg381_milestone2;
+
+/**
+ *
+ * @author squis
+ */
+public class PRG381_Milestone2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
