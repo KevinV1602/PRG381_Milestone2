2d7d47a0f9974d3a30c15cc08ae630a4b7ddc2be
Reagile Motsepe <reagilemotsepe@gmail.com>
2025-07-17 22:57:05 +0200

    Modified Feedback Model

--- PRG381_Milestone2/src/prg381_milestone2/Model/Feedback.java
+++ PRG381_Milestone2/src/prg381_milestone2/Model/Feedback.java
@@ -10,39 +10,32 @@ package prg381_milestone2.Model;
  */
 
 public class Feedback {
-    private int id;
     private String studentId;
     private String counselor;
     private int rating;
     private String comments;
 
-    // Constructors
     public Feedback() {}
     
-    public Feedback(String studentId, String counselor, int rating, String comments){
-    this.studentId = studentId;
-    this.counselor = counselor;
-    this.rating = rating;
-    this.comments = comments;
-    }
     
-    public Feedback(int id, String studentId, String counselor, int rating, String comments) {
-    this.id = id;
-    this.studentId = studentId;
-    this.counselor = counselor;
-    this.rating = rating;
-    this.comments = comments;
-}
 
-    // Getters and setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public Feedback(String studentId, String counselor, int rating, String comments) {
+        this.studentId = studentId;
+        this.counselor = counselor;
+        this.rating = rating;
+        this.comments = comments;
+    }
+
+    // Getters and Setters
     public String getStudentId() { return studentId; }
     public void setStudentId(String studentId) { this.studentId = studentId; }
+
     public String getCounselor() { return counselor; }
     public void setCounselor(String counselor) { this.counselor = counselor; }
+
     public int getRating() { return rating; }
     public void setRating(int rating) { this.rating = rating; }
+
     public String getComments() { return comments; }
     public void setComments(String comments) { this.comments = comments; }
 }
